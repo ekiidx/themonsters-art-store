fcfb1a2ef08f2d968c9198e84d51a28b74dc123c
ekiidx <info@vuedesign.co>
2023-08-28 01:33:47 -0400

    initial upload

---
+++ header.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<!-- Favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+	<meta name="msapplication-TileColor" content="#000000">
+	<meta name="theme-color" content="#000000">
+
+	<!-- Search Engine -->
+	<meta name="description" content="">
+	<meta name="image" content="">
+	<!-- Schema.org for Google -->
+	<meta itemprop="name" content="">
+	<meta itemprop="description" content="">
+	<meta itemprop="image" content="website-preview.jpg">
+	<!-- Open Graph general (Facebook, Pinterest & Google+) -->
+	<meta name="og:title" content="">
+	<meta name="og:description" content="">
+	<!--<meta name="og:image" content="">-->
+	<meta name="og:url" content="">
+	<meta name="og:site_name" content="">
+	<meta name="og:locale" content="en_US">
+	<meta name="og:type" content="website">
+	<meta property="og:image:width" content="1200">
+	<meta property="og:image:height" content="630">
+
+	<!--Bootstrap-->
+
+	<!-- Fonts -->
+                
+	<!-- Stylesheets -->
+    <?php $phpVersion = '?v=1.0.0'; ?>
+    <link rel="stylesheet" href="<?php echo '/assets/css/main.css' . $phpVersion; ?>">
+    <link rel="stylesheet" href="<?php echo '/assets/css/responsive.css' . $phpVersion; ?>">
+
+	<!--
+	 _   __           ___          _
+	| | / /_ _____   / _ \___ ___ (_)__ ____
+	| |/ / // / -_) / // / -_|_-</ / _ `/ _ \
+	|___/\_,_/\__/ /____/\__/___/_/\_, /_//_/
+                                  /___/
+	-->
+
+</head>
+
+<body>
+
+<header></header>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'header.php'; ?>
+
+<main id="home">
+
+</main>
+
+<?php include 'footer.php';
