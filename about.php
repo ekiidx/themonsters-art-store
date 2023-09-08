7bbb45402fdfb007639df38403013fa434e87fc3
ekiidx <info@vuedesign.co>
2023-09-08 17:53:52 -0400

    about.php added

---
+++ about.php
@@ -0,0 +1,37 @@
+<?php include 'header.php'; ?>
+
+<div id="about">
+
+    <section id="title" class="title">
+        <div class="row">
+            <h1>Weird and Welcome</h1>
+        </div>
+    </section>
+
+    <div id="main">
+        <div class="container">
+            <div class="item1">
+                <a href="#">
+                    <img src="/assets/img/coil.png">
+                </a>
+            </div>
+
+            <div class="item2">
+                <h2>Grrr.</h2>
+                <p>We often have to hide what makes us truly unique in order to fit into the world. The Monsters are here to make you feel good about the weirdo you naturally are.</p>
+
+                <p>Our organization is exploring the world of these fuzzy friends, discovering new and exciting variations all the time. Each Monster comes with its own shape, personality, and background—find the ones that fit you! They're not all smiles, but mostly they're pretty friendly.</p>
+
+                <p>From personalized commissions to stickers, murals, and installations, we are dedicated to making these vibrant Monsters accessible to everyone. We believe all people deserve to be able to own art that brings joy.</p>
+
+                <p>So tell us what you think, what you need, or how you feel. We are The Monsters, and we're here for you.</p>
+
+                <div class="random-signature">
+                    <img src="/assets/img/random-signature.svg">
+                </div>
+            </div>
+        </div>  
+    </div>
+</div>
+
+<?php include 'footer.php';
